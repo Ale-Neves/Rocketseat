628c67ab18d2ed5daf51d2ccbb2819969d39980a
Alexandre Neves <alexandreneves98@outlook.com>
2022-05-06 18:44:51 -0300

    06/05/2022 NLW Return Origin: Stage 3 – Novas Habilidades.

--- NLW Return/Origin/main.js
+++ NLW Return/Origin/main.js
@@ -1,8 +1,8 @@
 function onScroll() {
   if (scrollY > 0) {
-    idNavigation.classList.add('scroll')
+    navigation.classList.add('scroll')
   } else {
-    idNavigation.classList.remove('scroll')
+    navigation.classList.remove('scroll')
   }
 }
 
@@ -10,6 +10,21 @@ function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
-function clouseMenu() {
+function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
